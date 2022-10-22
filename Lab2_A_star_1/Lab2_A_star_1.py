2c575db9804facf6ac48bf5cf65d92ddab1a4ef3
Ганна Кушнір <anna.kushnir.1008@gmail.com>
2022-10-22 12:15:03 +0300

    trying to test the A* algorithm

--- Lab2_A_star_1/Lab2_A_star_1.py
+++ Lab2_A_star_1/Lab2_A_star_1.py
@@ -1,4 +1,13 @@
 from A_star import *
+import time
 
 if __name__ == "__main__":
-    pass
+    queens = [0, 6, 3, 3, 5, 0, 4, 0]
+    root = Node(queens)
+    start_time = time.time()
+    result = A_star(root)
+    print('Time: %s seconds' % (time.time() - start_time))
+    if result != None:
+        print('Solution: ', result.queens)
+    else:
+         print('Solution was not found')

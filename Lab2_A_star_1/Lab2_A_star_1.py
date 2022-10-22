335331bfc192d896c55f9e381e7b2c0e3ffe057d
Ганна Кушнір <anna.kushnir.1008@gmail.com>
2022-10-22 20:30:03 +0300

    added output of a checkerboard

--- Lab2_A_star_1/Lab2_A_star_1.py
+++ Lab2_A_star_1/Lab2_A_star_1.py
@@ -1,13 +1,29 @@
-from A_star import *
+﻿from A_star import *
 import time
 
+def printBoard(queens: list):
+    print('┌───┬───┬───┬───┬───┬───┬───┬───┐')
+    for i in range(8):
+        print('│' + '   │' * queens[i] + ' * │' + '   │' * (7 - queens[i]))
+        if i != 7:
+            print('├───┼───┼───┼───┼───┼───┼───┼───┤')
+        else:
+            print('└───┴───┴───┴───┴───┴───┴───┴───┘')
+
+
 if __name__ == "__main__":
-    queens = [0, 6, 3, 3, 5, 0, 4, 0]
+    queens = [0, 0, 0, 5, 7, 0, 4, 0]
+    print('Entry positions of queens: ', queens)
+    print('The input state of the checkerboard:')
+    printBoard(queens)
     root = Node(queens)
     start_time = time.time()
     result = A_star(root)
-    print('Time: %s seconds' % (time.time() - start_time))
+    end_time = time.time()
     if result != None:
         print('Solution: ', result.queens)
+        print('The resulting target state of the checkerboard:')
+        printBoard(result.queens)
     else:
-         print('Solution was not found')
+        print('Solution was not found')
+    print('Time spent: %0.5f seconds' % (end_time - start_time))
--- Lab2_IDFS_1/Lab2_IDFS_1.py
+++ Lab2_IDFS_1/Lab2_IDFS_1.py
@@ -1,11 +1,27 @@
-from IDFS import *
+﻿from IDFS import *
 import time
+
+def printBoard(queens: list):
+    print('┌───┬───┬───┬───┬───┬───┬───┬───┐')
+    for i in range(8):
+        print('│' + '   │' * queens[i] + ' * │' + '   │' * (7 - queens[i]))
+        if i != 7:
+            print('├───┼───┼───┼───┼───┼───┼───┼───┤')
+        else:
+            print('└───┴───┴───┴───┴───┴───┴───┴───┘')
+
 
 if __name__ == "__main__":
     queens = [0, 6, 3, 3, 5, 0, 4, 0]
+    print('Entry positions of queens: ', queens)
+    print('The input state of the checkerboard:')
+    printBoard(queens)
     root = Node(queens)
     start_time = time.time()
     result, depth = IDFS(root)
-    print('Time: %s seconds' % (time.time() - start_time))
+    end_time = time.time()
     print('Solution: ', result.queens)
-    print('Was found at a depth of', depth)
+    print('The resulting target state of the checkerboard:')
+    printBoard(result.queens)
+    print('Iterations:', depth)
+    print('Time spent: %0.5f seconds' % (end_time - start_time))

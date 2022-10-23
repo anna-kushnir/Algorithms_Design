5bc78cb380532163c954205d593136afca61e109
Ганна Кушнір <anna.kushnir.1008@gmail.com>
2022-10-23 12:37:31 +0300

    fixed inaccuracies in determining the maximum number of states in memory

--- Lab2_IDFS_2_Test/IDFS.py
+++ Lab2_IDFS_2_Test/IDFS.py
@@ -1,7 +1,6 @@
 from Node import *
 
 total_num_of_states = 1
-num_of_states_in_mem = 0
 
 def isAGoalState(queens: list):
     for i in range(NUM):
@@ -11,27 +10,21 @@ def isAGoalState(queens: list):
     return True
 
 def expand(node: Node):
-    global num_of_states_in_mem
+    global total_num_of_states
     newNode = Node(node.queens)
     for i in range (NUM):
         for j in range (1, NUM):
             newNode.addChild(i, j)
-    num_of_states_in_mem += NUM * (NUM - 1)
+    total_num_of_states += NUM * (NUM - 1)
     return newNode
 
 def IDFS(root: Node):
-    global total_num_of_states, num_of_states_in_mem
     depth = 0
-    max_num_of_states_in_mem = 0
     while True:
         result = DLS(root, depth)
-        total_num_of_states += num_of_states_in_mem
-        if num_of_states_in_mem > max_num_of_states_in_mem:
-            max_num_of_states_in_mem = num_of_states_in_mem
         if result != None and isAGoalState(result.queens):
-            return result, depth, total_num_of_states, max_num_of_states_in_mem + 1
+            return result, depth, total_num_of_states, depth * NUM * (NUM - 1)
         depth += 1
-        num_of_states_in_mem = 0
 
 def DLS(node: Node, depth: int):
     if depth == 0 and isAGoalState(node.queens):
